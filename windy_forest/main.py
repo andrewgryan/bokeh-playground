d1a4df9778179ecb074eb6b329df324335279cc4
andrewgryan <andrew.ryan@metoffice.gov.uk>
2018-06-08 15:26:06 +0100

    made left/right slider

--- windy_forest/main.py
+++ windy_forest/main.py
@@ -16,30 +16,46 @@ def main(bokeh_id):
 
 
 def windy_forest(figure):
-    left_renderer = figure.rect([1, 2, 3], [1, 2, 3], width=1, height=1,
-                                color="blue")
+    left_source = bokeh.models.ColumnDataSource({
+        "x": [1, 2, 3],
+        "y": [1, 2, 3],
+        "width": [1, 1, 1],
+        "alpha": [1, 1, 1]
+    })
+    left_renderer = figure.rect(x="x",
+                                y="y",
+                                width="width",
+                                height=1,
+                                color="blue",
+                                alpha="alpha",
+                                source=left_source)
+    hover_tool = hover_tool_hide(left_source, side="right")
+    figure.add_tools(hover_tool)
     right_source = bokeh.models.ColumnDataSource({
         "x": [1, 2, 3],
         "y": [3, 2, 1],
         "width": [0.5, 0.5, 0.5],
         "alpha": [1, 1, 1]
     })
-    right_renderer = figure.rect(x="x", y="y", width="width", height=0.5,
+    right_renderer = figure.rect(x="x",
+                                 y="y",
+                                 width="width",
+                                 height=0.5,
                                  color="red",
                                  alpha="alpha",
                                  source=right_source)
-    hover_tool = hover_tool_hide_left(right_source)
+    hover_tool = hover_tool_hide(right_source, side="left")
     figure.add_tools(hover_tool)
     vertical_line(figure)
 
-def hover_tool_hide_left(source):
-    """Hide anything to the left of pointer
+def hover_tool_hide(source, side="left"):
+    """Hide anything to the left/right of pointer
 
     At the moment this is achieved through the use of CustomJS and
     alpha values. A more complete solution would work on the canvas
     itself
     """
-    callback = bokeh.models.callbacks.CustomJS(args=dict(source=source), code="""
+    code_template = """
         let x_left, x_right;
         let x = source.data["x"];
         let width = source.data["width"];
@@ -48,9 +64,7 @@ def hover_tool_hide_left(source):
         for (let i=0; i<x.length; i++) {
             x_left = x[i] - (width[i] / 2);
             x_right = x[i] + (width[i] / 2);
-            if (x_right < mouse_x) {
-               alpha.push(0);
-            } else if ((x_left < mouse_x) & (mouse_x < x_right)) {
+            if (%s) {
                alpha.push(0);
             } else {
                alpha.push(1);
@@ -58,7 +72,13 @@ def hover_tool_hide_left(source):
         }
         source.data.alpha = alpha;
         source.change.emit();
-    """)
+    """
+    if side.lower() == "left":
+        code = code_template % "mouse_x < x_right"
+    else:
+        code = code_template % "mouse_x > x_left"
+    callback = bokeh.models.callbacks.CustomJS(args=dict(source=source),
+                                               code=code)
     return bokeh.models.HoverTool(callback=callback)
 
 
