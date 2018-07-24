b8451f01ee1821af77863acc5b405df3faf44f61
andrewgryan <andrew.ryan@metoffice.gov.uk>
2018-07-24 13:40:39 +0100

    applied common normalisation to multiple imshows

--- quad_mesh_to_rgba/coastlines.py
+++ quad_mesh_to_rgba/coastlines.py
@@ -1,3 +1,4 @@
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 import cartopy
@@ -5,10 +6,20 @@ import bokeh.plotting
 
 figure = bokeh.plotting.figure(sizing_mode='scale_width')
 
+# Coastlines
+for geometry in cartopy.feature.COASTLINE.geometries():
+    figure.line(*geometry[0].xy,
+                color='black',
+                level='overlay')
+
+# Custom normalisation
+norm = mpl.colors.Normalize(vmin=0,
+                            vmax=200)
+
 # Imshow
 ni, nj = 10, 10
 values = np.arange(ni * nj).reshape(ni, nj)
-mappable = plt.imshow(values)
+mappable = plt.imshow(values, norm=norm)
 rgba = mappable.to_rgba(mappable.get_array(),
                         bytes=True).reshape(ni, nj, 4)
 figure.image_rgba(image=[rgba],
@@ -17,9 +28,17 @@ figure.image_rgba(image=[rgba],
                   dh=ni,
                   dw=nj)
 
-# Coastlines
-for geometry in cartopy.feature.COASTLINE.geometries():
-    figure.line(*geometry[0].xy,
-                color='black')
+# Imshow
+ni, nj = 10, 10
+values = np.arange(ni * nj).reshape(ni, nj)
+values += 100
+mappable = plt.imshow(values, norm=norm)
+rgba = mappable.to_rgba(mappable.get_array(),
+                        bytes=True).reshape(ni, nj, 4)
+figure.image_rgba(image=[rgba],
+                  x=nj + 1,
+                  y=0,
+                  dh=ni,
+                  dw=nj)
 
 bokeh.plotting.show(figure)
