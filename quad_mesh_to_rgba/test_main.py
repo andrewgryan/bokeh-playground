e786ba11d99b6f338992bfde2d1374893ce75a49
andrewgryan <andrew.ryan@metoffice.gov.uk>
2018-07-24 11:36:28 +0100

    tidied up test suite

---
+++ quad_mesh_to_rgba/test_main.py
@@ -0,0 +1,15 @@
+import unittest
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class TestQuadMeshToBokehRGBA(unittest.TestCase):
+    """Mapping from matplotlib.pcolormesh to bokeh image_rgba"""
+    def test_quad_mesh_to_image_rgba(self):
+        values = np.ones((1, 1))
+        quad_mesh = plt.pcolormesh(values)
+        result = quad_mesh.to_rgba(quad_mesh.get_array(),
+                                   bytes=True).reshape((1, 1, 4))
+        expect = [[[68, 1, 84, 255]]]
+        np.testing.assert_array_almost_equal(result, expect)
+        self.assertEqual(result.dtype, np.uint8)
