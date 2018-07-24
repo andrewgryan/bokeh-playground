224cceafa4a7f9b966f71e01a28a1f9c43c3ee9e
andrewgryan <andrew.ryan@metoffice.gov.uk>
2018-07-24 16:09:12 +0100

    extracted coastlines method

--- quad_mesh_to_rgba/coastlines.py
+++ quad_mesh_to_rgba/coastlines.py
@@ -4,13 +4,39 @@ import numpy as np
 import cartopy
 import bokeh.plotting
 
-figure = bokeh.plotting.figure(sizing_mode='scale_width')
+figure = bokeh.plotting.figure(sizing_mode='stretch_both',
+                               match_aspect=True)
 
-# Coastlines
-for geometry in cartopy.feature.COASTLINE.geometries():
-    figure.line(*geometry[0].xy,
-                color='black',
-                level='overlay')
+
+# Struggling to find appropriate extent for cartopy feature
+# Left, right, bottom, top
+x0, x1, y0, y1 = (0, 21, 0, 10)
+extent = x0, x1, y0, y1
+
+
+def coastlines(figure, scale="110m", extent=None):
+    """Add cartopy coastline to a figure
+
+    Translates cartopy.feature.COASTLINE object
+    into collection of bokeh lines
+
+    .. note:: This method assumes the map projection
+              is cartopy.crs.PlateCarreee
+
+    :param figure: bokeh figure instance
+    :param scale: cartopy coastline scale '110m', '50m' or '10m'
+    :param extent: x_start, x_end, y_start, y_end
+    """
+    coastline = cartopy.feature.COASTLINE
+    coastline.scale = scale
+    for geometry in coastline.intersecting_geometries(extent):
+        figure.line(*geometry[0].xy,
+                    color='black',
+                    level='overlay')
+
+coastlines(figure,
+           scale="10m",
+           extent=extent)
 
 # Custom normalisation
 norm = mpl.colors.Normalize(vmin=0,
@@ -40,5 +66,11 @@ figure.image_rgba(image=[rgba],
                   y=0,
                   dh=ni,
                   dw=nj)
+
+# Set bokeh x/y range extents
+figure.x_range.start = x0
+figure.x_range.end = x1
+figure.y_range.start = y0
+figure.y_range.end = y1
 
 bokeh.plotting.show(figure)
