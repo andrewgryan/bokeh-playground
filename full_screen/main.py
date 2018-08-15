eaca8b3bbee7a69a581df0a5bcc6b6390ff2a0c3
andrewgryan <andrew.ryan@metoffice.gov.uk>
2018-08-15 15:16:09 +0100

    added aspect ratio rules

--- full_screen/main.py
+++ full_screen/main.py
@@ -1,5 +1,6 @@
 import bokeh.plotting
 document = bokeh.plotting.curdoc()
-figure = bokeh.plotting.figure()
+figure = bokeh.plotting.figure(match_aspect=True,
+                               aspect_scale=1)
 figure.circle([1, 2, 3], [1, 2, 3])
 document.add_root(figure)
