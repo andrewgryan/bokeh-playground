c8544db941306651c4fe7f9f1c2424856c158386
andrewgryan <andrew.ryan@metoffice.gov.uk>
2018-08-20 08:54:59 +0100

    split roots based on even/odd state

--- custom_button/main.py
+++ custom_button/main.py
@@ -1,6 +1,11 @@
 import bokeh.plotting
 import bokeh.models
 document = bokeh.plotting.curdoc()
-for i in range(5):
-    button = bokeh.models.Button(label="Button: {}".format(i))
+for i in range(10):
+    if i % 2 == 0:
+        name = "top"
+    else:
+        name = "bottom"
+    button = bokeh.models.Button(label="Button: {}".format(i),
+                                 name=name)
     document.add_root(button)
