e8b8657052c6b1636c8b6a485f23e07ee5635991
andrew.ryan <andrew.ryan@metoffice.gov.uk>
2018-09-06 14:18:52 +0100

    added support for toggling between layouts

--- one_to_two_figure/main.py
+++ one_to_two_figure/main.py
@@ -4,17 +4,41 @@ import bokeh.layouts
 
 document = bokeh.plotting.curdoc()
 
-figure_1 = bokeh.plotting.figure()
+figure_1 = bokeh.plotting.figure(plot_width=800)
 figure_1.circle([1, 2, 3], [1, 2, 3])
-figure_2 = bokeh.plotting.figure()
-figure_2.circle([1, 2, 3], [1, 2, 3], fill_color="red")
+figure_2 = bokeh.plotting.figure(plot_width=800)
+figure_2.circle([1, 2, 3], [1, 2, 3], fill_color="red", line_color=None)
+figure_3 = bokeh.plotting.figure(plot_width=1600)
+figure_3.circle([1, 2, 3], [1, 2, 3], fill_color="red", line_color=None)
 
-layout = bokeh.layouts.row(figure_1)
+layout = bokeh.layouts.row()
+
+button = bokeh.models.Button()
+design = "double"
+
+
+def render():
+    global design
+    # Layout figures
+    if design == "double":
+        layout.children = [figure_1, figure_2]
+    else:
+        layout.children = [figure_3]
+
+    # Update button label
+    button.label = design
+
+    # Toggle state
+    if design == "double":
+        design = "single"
+    else:
+        design = "double"
+
 
 def on_click():
-    layout.children = [figure_1, figure_2]
-button = bokeh.models.Button()
-button.on_click(on_click)
+    render()
 
+
+button.on_click(on_click)
 document.add_root(layout)
 document.add_root(button)
