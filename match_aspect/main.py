f2a8acf4a23ee214fb9ca96481ef9b6e82dbcc63
andrewgryan <andrew.ryan@metoffice.gov.uk>
2018-10-04 11:54:18 +0200

    moving towards generic fixed limit mechanism

--- match_aspect/main.py
+++ match_aspect/main.py
@@ -20,15 +20,19 @@ button.on_click(on_click)
 document.add_root(button)
 
 # Work-around to simulate rigid limits
+start = 0.9
+end = 3.1
 def on_click():
-    figure.x_range.start = 0.9
-    figure.x_range.end = 3.1
+    figure.x_range.start = start
+    figure.x_range.end = end
     figure.x_range.bounds = 'auto'
 button = bokeh.models.Button(label="Fix limits")
 button.on_click(on_click)
-js_fix_limits = bokeh.models.CustomJS(args=dict(figure=figure), code="""
-        figure.x_range._initial_start = 0.9;
-        figure.x_range._initial_end = 3.1;
+js_fix_limits = bokeh.models.CustomJS(args=dict(figure=figure,
+                                                _initial_start=start,
+                                                _initial_end=end), code="""
+        figure.x_range._initial_start = _initial_start;
+        figure.x_range._initial_end = _initial_end;
 """)
 button.js_on_click(js_fix_limits)
 
