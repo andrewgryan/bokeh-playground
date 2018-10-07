c16059382f1b751d8d9e3714880e4db49097989d
andrewgryan <andrew.ryan@metoffice.gov.uk>
2018-10-07 10:55:22 +0200

    introduced pub/sub model to organise bokeh widgets

--- plus_minus/main.py
+++ plus_minus/main.py
@@ -1,28 +1,44 @@
 import bokeh.plotting
 import bokeh.models
 
-p = bokeh.models.widgets.Paragraph(text="Hello, world!")
-values = [1, 2, 3]
-index = 0
 
-def plus():
-    global index
-    index += 1
-    print(values[index])
-    p.text = "Plus"
+class Paragraph(object):
+    def __init__(self, text):
+        self.text = text
+        self.text.register(self)
+        self.widget = bokeh.models.widgets.Paragraph(text=text.value)
 
-def minus():
-    global index
-    index -= 1
-    print(values[index])
-    p.text = "Minus"
+    def notify(self):
+        self.widget.text = self.text.value
 
+
+class Text(object):
+    def __init__(self):
+        self.value = ""
+        self.subscribers = []
+
+    def register(self, subscriber):
+        self.subscribers.append(subscriber)
+
+    def plus(self):
+        self.value = "Plus"
+        for subscriber in self.subscribers:
+            subscriber.notify()
+
+    def minus(self):
+        self.value = "Minus"
+        for subscriber in self.subscribers:
+            subscriber.notify()
+
+
+text = Text()
+paragraph = Paragraph(text)
 
 plus_btn = bokeh.models.Button(label="+")
-plus_btn.on_click(plus)
+plus_btn.on_click(text.plus)
 
 minus_btn = bokeh.models.Button(label="-")
-minus_btn.on_click(minus)
+minus_btn.on_click(text.minus)
 
 document = bokeh.plotting.curdoc()
-document.add_root(bokeh.layouts.row(p, plus_btn, minus_btn))
+document.add_root(bokeh.layouts.row(paragraph.widget, plus_btn, minus_btn))
