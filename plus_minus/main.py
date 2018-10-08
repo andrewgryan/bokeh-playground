ec54a3642b5fe6a506806a06c77a20ce37ef9ccf
andrewgryan <andrew.ryan@metoffice.gov.uk>
2018-10-08 12:28:54 +0200

    exploring more functional reactive programming concepts

--- plus_minus/main.py
+++ plus_minus/main.py
@@ -1,5 +1,6 @@
 import bokeh.plotting
 import bokeh.models
+from functools import partial
 
 
 class Paragraph(object):
@@ -28,6 +29,9 @@ class Stream(object):
     def scan(self, initial, combine):
         return Scan(self, initial, combine)
 
+    def filter(self, criteria):
+        return Filter(self, criteria)
+
 
 class Map(Stream):
     def __init__(self, stream, transform):
@@ -51,13 +55,44 @@ class Scan(Stream):
         self.emit(self.state)
 
 
+class Filter(Stream):
+    def __init__(self, stream, criteria):
+        self.criteria = criteria
+        stream.register(self)
+        super().__init__()
+
+    def notify(self, value):
+        if not self.criteria(value):
+            self.emit(value)
+
+
 def to_text(number):
     return "Number: {}".format(number)
 
 
+import datetime as dt
+def to_time(times, index):
+    return times[index % len(times)]
+
+
+def format_time(time):
+    return "{:%Y%m%d %H:%M}".format(time)
+
+
 def main():
+    times = [dt.datetime(2018, 1, 1),
+             dt.datetime(2018, 1, 2),
+             dt.datetime(2018, 1, 3)]
+
     numbers = Stream()
-    totals = numbers.scan(0, lambda x, y: x + y)
+    def list_bounds(items, total, value):
+        if (total + value) >= len(items):
+            return total
+        if (total + value) < 0:
+            return total
+        return total + value
+    time_bounds = partial(list_bounds, times)
+    totals = numbers.scan(0, time_bounds)
     text = totals.map(to_text)
     p = bokeh.models.widgets.Paragraph(text="")
     Paragraph(p, text)
@@ -68,7 +103,13 @@ def main():
     minus_btn = bokeh.models.Button(label="-")
     minus_btn.on_click(lambda: numbers.emit(-1))
 
+    time_getter = partial(to_time, times)
+    text = totals.map(time_getter).map(format_time)
+    current_p = bokeh.models.widgets.Paragraph(text="")
+    current_date = Paragraph(current_p, text)
+
     document = bokeh.plotting.curdoc()
+    document.add_root(current_p)
     document.add_root(bokeh.layouts.row(p, plus_btn, minus_btn))
 
 
