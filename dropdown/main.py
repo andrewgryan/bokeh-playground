27d8e0a186baa10b28a31ddb28306f51a1f7166d
andrewgryan <andrew.ryan@metoffice.gov.uk>
2018-10-16 14:45:49 +0100

    moving towards functional

--- dropdown/main.py
+++ dropdown/main.py
@@ -1,14 +1,17 @@
 import bokeh.plotting
 import bokeh.layouts
-menu = [
-    ("A", "a"),
-    ("B", "b"),
-]
-dropdown = bokeh.models.Dropdown(menu=menu)
+
+letters = ["A", "B"]
+
+def as_menu(letters):
+    return [(letter, letter.lower()) for letter in letters]
+dropdown = bokeh.models.Dropdown(menu=as_menu(letters))
 dropdown.on_click(lambda value: print(value))
-button = bokeh.models.Button()
+
 def on_click():
-    dropdown.menu = [("C", "c")]
+    dropdown.menu = as_menu(["C"])
+button = bokeh.models.Button()
 button.on_click(on_click)
+
 document = bokeh.plotting.curdoc()
 document.add_root(bokeh.layouts.row(button, dropdown))
