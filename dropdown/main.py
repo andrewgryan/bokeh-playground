6072d3fef63f031ebc95a96e90e694f12c686f81
andrewgryan <andrew.ryan@metoffice.gov.uk>
2018-10-16 14:53:23 +0100

    switched to reactive style

--- dropdown/main.py
+++ dropdown/main.py
@@ -1,17 +1,22 @@
+import forest
 import bokeh.plotting
 import bokeh.layouts
 
-letters = ["A", "B"]
+stream = forest.Stream()
+menu_stream = stream.map(lambda c: (c, c.lower())).scan([], lambda a, i: a + [i])
 
-def as_menu(letters):
-    return [(letter, letter.lower()) for letter in letters]
-dropdown = bokeh.models.Dropdown(menu=as_menu(letters))
+dropdown = bokeh.models.Dropdown()
 dropdown.on_click(lambda value: print(value))
+def update(menu):
+    dropdown.menu = menu
+menu_stream.subscribe(update)
 
 def on_click():
-    dropdown.menu = as_menu(["C"])
+    stream.emit("C")
 button = bokeh.models.Button()
 button.on_click(on_click)
+stream.emit("A")
+stream.emit("B")
 
 document = bokeh.plotting.curdoc()
 document.add_root(bokeh.layouts.row(button, dropdown))
