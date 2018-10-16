f3af42ffc724c7f0f7020ff90b2b8bb3e31aaca0
andrewgryan <andrew.ryan@metoffice.gov.uk>
2018-10-16 15:01:02 +0100

    added more streams

--- dropdown/main.py
+++ dropdown/main.py
@@ -2,8 +2,11 @@ import forest
 import bokeh.plotting
 import bokeh.layouts
 
+
 stream = forest.Stream()
-menu_stream = stream.map(lambda c: (c, c.lower())).scan([], lambda a, i: a + [i])
+def menu_item(word):
+    return word, word.lower()
+menu_stream = stream.map(menu_item).scan([], lambda a, i: a + [i])
 
 dropdown = bokeh.models.Dropdown()
 dropdown.on_click(lambda value: print(value))
@@ -11,12 +14,21 @@ def update(menu):
     dropdown.menu = menu
 menu_stream.subscribe(update)
 
+buttons = []
 def on_click():
     stream.emit("C")
 button = bokeh.models.Button()
 button.on_click(on_click)
+buttons.append(button)
+
+def on_click():
+    stream.emit("D")
+button = bokeh.models.Button()
+button.on_click(on_click)
+buttons.append(button)
+
 stream.emit("A")
 stream.emit("B")
 
 document = bokeh.plotting.curdoc()
-document.add_root(bokeh.layouts.row(button, dropdown))
+document.add_root(bokeh.layouts.row(*buttons, dropdown))
