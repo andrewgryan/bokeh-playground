d9b313d8f3c6360fc66d84b000ae8b9325d5d0ed
andrewgryan <andrew.ryan@metoffice.gov.uk>
2018-10-21 20:07:01 +0100

    added a static file handler to serve style.css

--- tornado/main.py
+++ tornado/main.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python
+import os
 import tornado.ioloop
 import tornado.web
 
 
 class Index(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world!")
+        self.render("index.html")
 
 
 def main():
+    static_path = os.path.join(os.path.dirname(__file__), 'static')
     return tornado.web.Application([
         (r"/", Index)
-    ])
+    ],
+    static_path=static_path)
 
 
 if __name__ == '__main__':
+    port = 5006
     app = main()
-    app.listen(5006)
+    app.listen(port)
+    print("serving localhost:{}".format(port))
     tornado.ioloop.IOLoop.current().start()
