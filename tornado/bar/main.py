e8a10e26d599ad6b5b97cc4d6b23eb913daaded2
andrew.ryan <andrew.ryan@metoffice.gov.uk>
2018-10-23 17:50:15 +0100

    regular commit

--- tornado/bar/main.py
+++ tornado/bar/main.py
@@ -4,6 +4,10 @@ import bokeh.themes
 import jinja2
 
 
+script_dir = os.path.dirname(__file__)
+env = jinja2.Environment(loader=jinja2.FileSystemLoader(script_dir))
+
+
 def app(document):
     figure = bokeh.plotting.figure(sizing_mode="stretch_both")
     figure.circle([1, 2, 3], [1, 2, 3])
@@ -19,9 +23,4 @@ def app(document):
     filename = os.path.join(os.path.dirname(__file__), "theme.yaml")
     document.theme = bokeh.themes.Theme(filename=filename)
 
-    document.template = jinja2.Template("""
-{% extends base %}
-{% block contents %}
-   <h1>Hello, Jinja2</h1>
-{% endblock %}
-""")
+    document.template = env.get_template("templates/index.html")
