1b0735fdc7379fcc27a163a1162a6fe2a3be4e31
andrewgryan <andrew.ryan@metoffice.gov.uk>
2018-10-29 13:19:57 +0000

    more asyncio examples

--- asyncio_example/main.py
+++ asyncio_example/main.py
@@ -5,14 +5,9 @@ async def say_after(delay, message):
     await asyncio.sleep(delay)
     print(message)
 
-async def main():
-    print(f"started: {time.strftime('%X')}")
-    await say_after(1, "Hello, ...")
-    print(f"middle: {time.strftime('%X')}")
-    await say_after(2, "...world!")
-    print(f"finished: {time.strftime('%X')}")
-
-
 loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
+loop.run_until_complete(asyncio.gather(
+        say_after(3, "Hello, ..."),
+        say_after(2, "...world!")
+    ))
 loop.close()
