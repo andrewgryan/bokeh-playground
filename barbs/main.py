b937f7e0f13fbbabc2330acded1691e3ec033ec4
andrewgryan <andrew.ryan@metoffice.gov.uk>
2018-11-09 09:44:41 +0000

    visual testing of bokeh barbs

---
+++ barbs/main.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import matplotlib.quiver
+import numpy as np
+import bokeh.plotting
+import forest
+
+figure = bokeh.plotting.figure(
+        match_aspect=True,
+        sizing_mode="stretch_both")
+
+ax = plt.gca()
+angle = 30
+C = 65
+U = C * np.cos(np.deg2rad(angle))
+V = C * np.sin(np.deg2rad(angle))
+mpl_barbs = matplotlib.quiver.Barbs(ax, U, V)
+
+xs, ys = forest.bokeh_barbs(mpl_barbs)
+figure.multi_line(xs=xs, ys=ys)
+
+document = bokeh.plotting.curdoc()
+document.add_root(figure)
