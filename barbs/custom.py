2dc44afc59e6c4c04112e44200c8c5af4f50035e
andrewgryan <andrew.ryan@metoffice.gov.uk>
2018-11-09 16:14:31 +0000

    removed dependency on Triangle class

--- barbs/custom.py
+++ barbs/custom.py
@@ -1,8 +1,8 @@
-from bokeh.models import Triangle
+from bokeh.models import Marker
 from bokeh.core.properties import Seq, Float
 
 
-class Barbs(Triangle):
+class Barbs(Marker):
     __implementation__ = "custom.ts"
     xb = Seq(Float())
     yb = Seq(Float())
