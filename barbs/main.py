5db5a04780ec7bfc48c3630caa3e11a82a6c5c41
andrewgryan <andrew.ryan@metoffice.gov.uk>
2018-11-12 10:21:22 +0000

    finding bottleneck by comparing with bokeh circles

--- barbs/main.py
+++ barbs/main.py
@@ -17,19 +17,6 @@ def timed(func):
         return result
     return wrapper
 
-figure = bokeh.plotting.figure(
-        match_aspect=True,
-        sizing_mode="stretch_both")
-
-ax = plt.gca()
-angle = 30
-
-X = np.arange(-10, 10, 1, dtype="d")
-Y = np.arange(-10, 10, 1, dtype="d")
-U, V = np.meshgrid(X**2, Y**2)
-
-mpl_barbs = matplotlib.quiver.Barbs(ax, X, Y, U, V)
-
 @timed
 def bokeh_barbs(mpl_barb):
     """Convert matplotlib.quiver.Barbs to bokeh multiline/patches data"""
@@ -42,20 +29,48 @@ def bokeh_barbs(mpl_barb):
         ys.append(y)
     return xo, yo, xs, ys
 
-x, y, a, b = bokeh_barbs(mpl_barbs)
-glyph = custom.Barbs(
-        x="x",
-        y="y",
-        a="a",
-        b="b",
-        size=10)
-source = bokeh.models.ColumnDataSource(dict(
-        x=x,
-        y=y,
-        a=a,
-        b=b,
-    ))
-figure.add_glyph(source, glyph)
+
+figure = bokeh.plotting.figure(
+        match_aspect=True,
+        sizing_mode="stretch_both")
+
+mode = "circles"
+if mode == "barbs":
+    ax = plt.gca()
+    angle = 30
+
+    X = np.arange(-10, 10, 1, dtype="d")
+    Y = np.arange(-10, 10, 1, dtype="d")
+    U, V = np.meshgrid(X**2, Y**2)
+
+    mpl_barbs = matplotlib.quiver.Barbs(ax, X, Y, U, V)
+    x, y, a, b = bokeh_barbs(mpl_barbs)
+    glyph = custom.Barbs(
+            x="x",
+            y="y",
+            a="a",
+            b="b",
+            size=10)
+    source = bokeh.models.ColumnDataSource(dict(
+            x=x,
+            y=y,
+            a=a,
+            b=b,
+        ))
+    figure.add_glyph(source, glyph)
+else:
+    source = bokeh.models.ColumnDataSource(dict(
+            x=[],
+            y=[],
+            r=[],
+        ))
+    glyph = bokeh.models.Circle(
+            x="x",
+            y="y",
+            radius="r"
+            )
+    figure.add_glyph(source, glyph)
+
 
 # Demonstrate click/change barbs
 def on_click():
@@ -73,7 +88,7 @@ def on_click():
 
 
 @timed
-def rotate():
+def rotate_barbs():
     global X, Y, U, V, source
     angle = np.rad2deg(np.arctan2(V, U))
     angle += 5
@@ -95,10 +110,29 @@ def rotate():
         "b": b,
     }
 
+
+@timed
+def random_circles():
+    global source
+    n = 10000
+    x = np.random.random(n) * 10
+    y = np.random.random(n) * 10
+    r = np.random.random(n) / 5
+    source.data = {
+        "x": x,
+        "y": y,
+        "r": r
+    }
+
 button = bokeh.models.Button(label="Change barbs")
 button.on_click(on_click)
 
 document = bokeh.plotting.curdoc()
 document.add_root(button)
 document.add_root(figure)
-document.add_periodic_callback(rotate, 50)
+duration = 50
+if mode == "circles":
+    callback = random_circles
+else:
+    callback = rotate_barbs
+document.add_periodic_callback(callback, duration)
