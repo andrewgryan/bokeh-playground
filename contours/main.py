a38ba4ed551b8340b5c1c137417fe85225de2ab9
andrewgryan <andrew.ryan@metoffice.gov.uk>
2018-11-26 16:45:42 +0000

    added an example to pick up colors

--- contours/main.py
+++ contours/main.py
@@ -3,13 +3,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.contour
 
+
+def rgba2hex(rgba):
+    assert len(rgba) == 4, "can only process RGBA values"
+    values = np.floor(rgba * 255).astype(np.int)
+    return "#{:02x}{:02x}{:02x}{:02x}".format(*values)
+
+
 figure = bokeh.plotting.figure(
         sizing_mode="stretch_both")
 
 x = np.linspace(-10, 10, 100)
 y = np.linspace(-10, 10, 100)
 X, Y = np.meshgrid(x, y)
-Z = X**2 + Y
+Z = np.cos(X) + np.sin(Y)
 
 ax = plt.gca()
 qcs = matplotlib.contour.QuadContourSet(ax, X, Y, Z)
@@ -29,18 +36,21 @@ for t in qcs.labelTexts:
     yl.append(y)
 angles = np.deg2rad([t.get_rotation() for t in qcs.labelTexts])
 texts = [t.get_text() for t in qcs.labelTexts]
+colors = [rgba2hex(c.get_color()[0]) for c in qcs.collections]
 source = bokeh.models.ColumnDataSource(dict(
         x=xl,
         y=yl,
         text=texts,
-        angle=angles))
+        angle=angles,
+        text_color=colors))
 print(angles, texts)
 labels = bokeh.models.LabelSet(
         x="x",
         y="y",
         text="text",
         angle="angle",
-        text_font_size="10px",
+        text_color="text_color",
+        text_font_size="9px",
         text_align="center",
         text_baseline="middle",
         background_fill_color="white",
