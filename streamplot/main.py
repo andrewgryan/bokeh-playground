8c7bc4cbf4d2ecca49287ebeccbfdd38d7a0e72d
andrewgryan <andrew.ryan@metoffice.gov.uk>
2018-11-30 17:10:21 +0000

    simple streamplot example

---
+++ streamplot/main.py
@@ -0,0 +1,20 @@
+import numpy as np
+import bokeh.plotting
+import matplotlib.pyplot as plt
+
+x = np.linspace(0, 1, 3)
+y = np.linspace(0, 1, 3)
+u, v = np.meshgrid(x, y)
+
+artist = plt.streamplot(x, y, u, v)
+
+bokeh_figure = bokeh.plotting.figure(sizing_mode="stretch_both")
+xs = []
+ys = []
+for path in artist.lines.get_paths():
+    x, y = path.vertices.T
+    xs.append(x)
+    ys.append(y)
+bokeh_figure.multi_line(xs=xs, ys=ys)
+document = bokeh.plotting.curdoc()
+document.add_root(bokeh_figure)
