4bdbd6c270ba253d73bd0291c2208b15423608b5
andrew.ryan <andrew.ryan@metoffice.gov.uk>
2018-11-30 11:36:10 +0000

    added more tests to understand map_coordinates

--- mercator/test_stretch.py
+++ mercator/test_stretch.py
@@ -5,60 +5,59 @@ import numpy as np
 
 
 class TestMapCoordinates(unittest.TestCase):
-    def test_scipy_ndimage_map_coordinates(self):
-        values = np.array([
+    def setUp(self):
+        self.values = np.array([
             [0, 1, 2],
             [3, 4, 5]
-        ])
+        ], dtype=np.float)
+
+    def test_scipy_ndimage_map_coordinates(self):
         i = [1]
         j = [2]
-        result = scipy.ndimage.map_coordinates(values, (i, j))
+        result = scipy.ndimage.map_coordinates(self.values, (i, j))
         expect = 5
         np.testing.assert_array_almost_equal(expect, result)
 
     def test_scipy_ndimage_map_coordinates_given_all_indices(self):
-        values = np.array([
-            [0, 1, 2],
-            [3, 4, 5]
-        ])
         i = [[0, 0, 0],
              [1, 1, 1]]
         j = [[0, 1, 2],
              [0, 1, 2]]
-        result = scipy.ndimage.map_coordinates(values, (i, j))
-        expect = values
+        result = scipy.ndimage.map_coordinates(self.values, (i, j))
+        expect = self.values
         np.testing.assert_array_almost_equal(expect, result)
 
-    def test_scipy_ndimage_map_coordinates_given_non_integer_index(self):
-        values = np.array([
-            [0, 1, 2],
-            [3, 4, 5]
-        ], dtype=np.float)
-        i = [[0, 0, 0],
-             [0.5, 0.5, 0.5]]
-        j = [[0, 1, 2],
-             [0, 1, 2]]
-        result = scipy.ndimage.map_coordinates(values, (i, j))
-        expect = np.array([
-            [0, 1, 2],
-            [1.5, 2.5, 3.5]
-        ])
-        np.testing.assert_array_almost_equal(expect, result)
+    def test_scipy_ndimage_map_coordinates_no_spline_filter(self):
+        """
+        A spline filter is only used when order is greater than 1
 
-    def test_scipy_ndimage_map_coordinates_given_different_axis(self):
-        values = np.array([
-            [0, 1, 2],
-            [3, 4, 5]
-        ], dtype=np.float)
+        Calling map_coordinates with order=1 is equivalent to
+        a bilinear interpolator
+        """
         i = np.array([[0, 0, 0],
                       [1, 1, 1]], dtype=np.float)
         j = np.array([[0, 0.5, 2],
                       [0, 1, 2]], dtype=np.float)
-        result = scipy.ndimage.map_coordinates(values, (i, j), order=1)
+        result = scipy.ndimage.map_coordinates(self.values, (i, j), order=1)
         expect = np.array([
             [0, 0.5, 2],
             [3, 4, 5]
         ], dtype=np.float)
+        np.testing.assert_array_almost_equal(expect, result)
+
+    def test_scipy_ndimage_map_coordinates_bilinear_example(self):
+        """order=1 equivalent to bilinear interpolation
+
+        0---1---2
+        | x |   |
+        3---4---5
+
+        x = 2 under bilinear interpolation
+        """
+        i = np.array([[0.5]], dtype=np.float)
+        j = np.array([[0.5]], dtype=np.float)
+        result = scipy.ndimage.map_coordinates(self.values, (i, j), order=1)
+        expect = np.array([[2.]], dtype=np.float)
         np.testing.assert_array_almost_equal(expect, result)
 
 
