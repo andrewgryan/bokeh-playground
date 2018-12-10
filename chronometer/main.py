cc3553abdb884a8e33be277de8ce266e19580511
andrewgryan <andrew.ryan@metoffice.gov.uk>
2018-12-10 15:57:43 +0000

    connected widget to real data

--- chronometer/main.py
+++ chronometer/main.py
@@ -39,6 +39,7 @@ figure.yaxis.axis_label_text_font_size = "8px"
 figure.yaxis.ticker = [0, 12, 24, 48]
 
 # Display all dates
+div = bokeh.models.Div()
 for d in dates:
     y = np.array([3 * i for i in range(12)])
     x = np.array([d + dt.timedelta(hours=float(h))
@@ -61,10 +62,26 @@ for d in dates:
             fill_color="grey",
             line_color=None)
 
+    def on_change(source, div):
+        def wrapper(attr, old, new):
+            if len(new) > 0:
+                i = new[0]
+                x, y, s = (
+                        source.data["x"][i],
+                        source.data["y"][i],
+                        source.data["s"][i])
+                msg = "<p>Valid date: {}</p><p>Start date: {}</p><p>Offset: {}</p>".format(
+                        dt.datetime.fromtimestamp(x / 1000.),
+                        dt.datetime.fromtimestamp(s / 1000.),
+                      "T+{}".format(str(y)))
+                div.text = msg
+        return wrapper
+    source.selected.on_change('indices', on_change(source, div))
+
 
 widgets = [
+        div,
         bokeh.models.DatePicker(),
-        bokeh.models.Dropdown()
         ]
 document.add_root(bokeh.layouts.widgetbox(*widgets))
 document.add_root(figure)
