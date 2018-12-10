1d6af83413d73248821975664e1145e6c7d31400
andrewgryan <andrew.ryan@metoffice.gov.uk>
2018-12-10 14:07:45 +0000

    experimenting with existing bokeh widgets

--- chronometer/main.py
+++ chronometer/main.py
@@ -1,6 +1,18 @@
 import bokeh.plotting
-import chronometer
+import bokeh.models
 
-widget = chronometer.Chronometer()
+widgets = [
+        bokeh.models.DatePicker(),
+        bokeh.models.Slider(
+            value=1.5,
+            start=1.5,
+            end=13.5,
+            step=3),
+        bokeh.models.RangeSlider(
+            value=(0, 3),
+            start=0,
+            end=12,
+            step=3),
+        ]
 document = bokeh.plotting.curdoc()
-document.add_root(widget)
+document.add_root(bokeh.layouts.column(*widgets))
