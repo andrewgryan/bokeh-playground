da2552823ed6360f5e5ce9bb1f1db277be298995
andrew.ryan <andrew.ryan@metoffice.gov.uk>
2018-12-19 10:51:34 +0000

    added a web map tiling service to both figures

--- one_to_two_figure/main.py
+++ one_to_two_figure/main.py
@@ -5,21 +5,33 @@ import bokeh.layouts
 
 def main():
     document = bokeh.plotting.curdoc()
+    tile = bokeh.models.WMTSTileSource(
+        url='http://c.tile.openstreetmap.org/{Z}/{X}/{Y}.png',
+        attribution="Attribution text goes here"
+    )
     first = bokeh.plotting.figure(
+        x_range=(-2000000, 6000000),
+        y_range=(-1000000, 7000000),
+        x_axis_type="mercator",
+        y_axis_type="mercator",
         active_scroll="wheel_zoom")
     second = bokeh.plotting.figure(
         active_scroll="wheel_zoom",
+        x_axis_type="mercator",
+        y_axis_type="mercator",
         x_range=first.x_range,
         y_range=first.y_range)
     figures = [
      first,
      second
     ]
-    figures[0].circle([1, 2, 3], [1, 2, 3])
-    figures[1].circle([1, 2, 3], [1, 2, 3], fill_color="red", line_color=None)
     for f in figures:
         f.toolbar.logo = None
         f.toolbar_location = None
+        f.add_tile(tile)
+
+    figures[0].circle([1, 2, 3], [1, 2, 3])
+    figures[1].circle([1, 2, 3], [1, 2, 3], fill_color="red", line_color=None)
 
     layout = bokeh.layouts.row(*figures, sizing_mode="stretch_both")
     layout.children = [figures[0]]  # Trick to keep correct sizing modes
