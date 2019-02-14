327bf17906511121ec10fb2d0c44fc3beefcd3ca
andrew.ryan <andrew.ryan@metoffice.gov.uk>
2019-02-14 13:36:43 +0000

    added more menus

--- stats_viewer/main.py
+++ stats_viewer/main.py
@@ -6,14 +6,14 @@ import numpy as np
 import glob
 
 
-STATS_FILES = sorted(glob.glob("/scratch/oceanver/class4/copernicus/glo/stats/product_quality_stats_GLOBAL_ANALYSIS_FORECAST_PHYS_001_015_*.nc"))
+STATS_FILES = sorted(glob.glob("/scratch/oceanver/class4/copernicus/*/stats/product_quality_*.nc"))
 
 
 def main():
     figure = bokeh.plotting.figure(
         x_axis_type="datetime",
-        plot_width=800,
-        plot_height=200)
+        plot_width=1200,
+        plot_height=300)
     figure.toolbar_location = "below"
     hover_tool = bokeh.models.HoverTool(
         tooltips=[
@@ -32,25 +32,36 @@ def main():
 
     dropdowns = []
 
+    model = Model()
+
     view = View(source)
+    model.register(view)
+
+    title = Title(figure)
+    model.register(title)
+
+    items = find_attributes(STATS_FILES, "product")
+    menu = [(item.strip(), item) for item in items]
+    dropdown = bokeh.models.Dropdown(menu=menu, label="Products")
+    dropdown.on_click(model.on_experiment)
+    dropdowns.append(dropdown)
 
     items = find_variables(STATS_FILES)
     menu = [(item.strip(), item) for item in items]
     dropdown = bokeh.models.Dropdown(menu=menu, label="Variables")
-    dropdown.on_click(view.on_variable)
+    dropdown.on_click(model.on_variable)
     dropdowns.append(dropdown)
 
     items = find_names(STATS_FILES, "metric_names")
     menu = [(item.strip(), item) for item in items]
     dropdown = bokeh.models.Dropdown(menu=menu, label="Metrics")
-    dropdown.on_click(view.on_metric)
-    dropdown.on_click(set_title(figure))
+    dropdown.on_click(model.on_metric)
     dropdowns.append(dropdown)
 
     items = find_names(STATS_FILES, "area_names")
     menu = [(item.strip(), item) for item in items]
     dropdown = bokeh.models.Dropdown(menu=menu, label="Regions")
-    dropdown.on_click(view.on_region)
+    dropdown.on_click(model.on_region)
     dropdowns.append(dropdown)
 
     row = bokeh.layouts.row(dropdowns)
@@ -59,43 +70,87 @@ def main():
     document.add_root(root)
 
 
-class View(object):
-    def __init__(self, source):
-        self.source = source
+class Model(object):
+    def __init__(self):
+        self.experiment = None
         self.metric = None
         self.variable = None
         self.region = None
+        self.subscribers = []
+
+    def register(self, subscriber):
+        self.subscribers.append(subscriber)
+
+    def on_experiment(self, value):
+        self.experiment = value
+        self.notify()
 
     def on_metric(self, value):
         self.metric = value
-        self.render()
+        self.notify()
 
     def on_variable(self, value):
         self.variable = value
-        self.render()
+        self.notify()
 
     def on_region(self, value):
         self.region = value
-        self.render()
+        self.notify()
 
-    def render(self):
-        if self.metric is None:
-            return
-        if self.variable is None:
-            return
-        if self.region is None:
-            return
-        x, y = read(self.variable, self.metric, self.region)
+    def notify(self):
+        for subscriber in self.subscribers:
+            subscriber.notify(self)
+
+
+class View(object):
+    def __init__(self, source):
+        self.source = source
+
+    def notify(self, model):
+        if self.valid(model):
+            self.render(model)
+
+    def valid(self, model):
+        for attr in ["experiment", "metric", "variable", "region"]:
+            if getattr(model, attr) is None:
+                return False
+        return True
+
+    def render(self, model):
+        x, y = read(select(STATS_FILES, "product", model.experiment),
+                    model.variable,
+                    model.metric,
+                    model.region)
         self.source.data = {
             "x": x,
             "y": y
         }
 
 
-def set_title(figure):
-    def wrapper(new):
-        figure.title.text = new
-    return wrapper
+class Title(object):
+    def __init__(self, figure):
+        self.figure = figure
+        self.title = bokeh.models.Title(text_font_size="12pt")
+        self.suptitle = bokeh.models.Title(text_font_style="italic")
+        self.figure.add_layout(self.suptitle, "above")
+        self.figure.add_layout(self.title, "above")
+
+    def notify(self, model):
+        self.render(model)
+
+    def render(self, model):
+        words = []
+        for attr in ["experiment"]:
+            if getattr(model, attr) is None:
+                continue
+            words.append(getattr(model, attr).strip())
+        self.title.text = "\n".join(words)
+        words = []
+        for attr in ["variable", "metric", "region"]:
+            if getattr(model, attr) is None:
+                continue
+            words.append(getattr(model, attr).strip())
+        self.suptitle.text = "\n".join(words)
 
 
 def find_names(paths, variable):
@@ -109,6 +164,19 @@ def find_names(paths, variable):
                     metrics.append(name)
     return metrics
 
+
+def find_attributes(paths, attr):
+    values = []
+    for path in paths:
+        with netCDF4.Dataset(path) as dataset:
+            if not hasattr(dataset, attr):
+                continue
+            value = getattr(dataset, attr)
+            if value not in values:
+                values.append(value)
+    return values
+
+
 def find_variables(paths):
     variables = []
     for path in paths:
@@ -120,10 +188,17 @@ def find_variables(paths):
     return variables
 
 
-def read(variable, metric, area):
+def select(paths, attr, value):
+    for path in paths:
+        with netCDF4.Dataset(path) as dataset:
+            if getattr(dataset, attr) == value:
+                yield path
+
+
+def read(paths, variable, metric, area):
     ys = []
     xs = []
-    for path in STATS_FILES:
+    for path in paths:
         with netCDF4.Dataset(path) as dataset:
             print("reading: {}".format(path))
             var = dataset.variables["time"]
