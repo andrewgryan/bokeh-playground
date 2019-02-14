4e66ed6c676562449040626ffd1a4c31f50ecfbd
andrew.ryan <andrew.ryan@metoffice.gov.uk>
2019-02-14 11:43:27 +0000

    added more menus

--- stats_viewer/main.py
+++ stats_viewer/main.py
@@ -14,6 +14,7 @@ def main():
         x_axis_type="datetime",
         plot_width=800,
         plot_height=200)
+    figure.toolbar_location = "below"
     hover_tool = bokeh.models.HoverTool(
         tooltips=[
             ('date', '@x{%F}'),
@@ -29,32 +30,66 @@ def main():
     figure.line(x="x", y="y", source=source)
     figure.circle(x="x", y="y", source=source)
 
-    metrics = []
-    for path in STATS_FILES:
-        with netCDF4.Dataset(path) as dataset:
-            chars = dataset.variables["metric_names"][:]
-            names = netCDF4.chartostring(chars)
-            for name in names:
-                if name not in metrics:
-                    metrics.append(name)
-    menu = [(item.strip(), item) for item in metrics]
-    dropdown = bokeh.models.Dropdown(menu=menu)
-    dropdown.on_click(select_metric(source))
-    dropdown.on_click(set_title(figure))
+    dropdowns = []
 
-    root = bokeh.layouts.column(dropdown, figure)
+    view = View(source)
+
+    items = find_variables(STATS_FILES)
+    menu = [(item.strip(), item) for item in items]
+    dropdown = bokeh.models.Dropdown(menu=menu, label="Variables")
+    dropdown.on_click(view.on_variable)
+    dropdowns.append(dropdown)
+
+    items = find_names(STATS_FILES, "metric_names")
+    menu = [(item.strip(), item) for item in items]
+    dropdown = bokeh.models.Dropdown(menu=menu, label="Metrics")
+    dropdown.on_click(view.on_metric)
+    dropdown.on_click(set_title(figure))
+    dropdowns.append(dropdown)
+
+    items = find_names(STATS_FILES, "area_names")
+    menu = [(item.strip(), item) for item in items]
+    dropdown = bokeh.models.Dropdown(menu=menu, label="Regions")
+    dropdown.on_click(view.on_region)
+    dropdowns.append(dropdown)
+
+    row = bokeh.layouts.row(dropdowns)
+    root = bokeh.layouts.column(row, figure)
     document = bokeh.plotting.curdoc()
     document.add_root(root)
 
-def select_metric(source):
-    def wrapper(new):
-        print(new)
-        x, y = read_metric(new)
-        source.data = {
+
+class View(object):
+    def __init__(self, source):
+        self.source = source
+        self.metric = None
+        self.variable = None
+        self.region = None
+
+    def on_metric(self, value):
+        self.metric = value
+        self.render()
+
+    def on_variable(self, value):
+        self.variable = value
+        self.render()
+
+    def on_region(self, value):
+        self.region = value
+        self.render()
+
+    def render(self):
+        if self.metric is None:
+            return
+        if self.variable is None:
+            return
+        if self.region is None:
+            return
+        x, y = read(self.variable, self.metric, self.region)
+        self.source.data = {
             "x": x,
             "y": y
         }
-    return wrapper
 
 
 def set_title(figure):
@@ -63,7 +98,29 @@ def set_title(figure):
     return wrapper
 
 
-def read_metric(metric):
+def find_names(paths, variable):
+    metrics = []
+    for path in paths:
+        with netCDF4.Dataset(path) as dataset:
+            chars = dataset.variables[variable][:]
+            names = netCDF4.chartostring(chars)
+            for name in names:
+                if name not in metrics:
+                    metrics.append(name)
+    return metrics
+
+def find_variables(paths):
+    variables = []
+    for path in paths:
+        with netCDF4.Dataset(path) as dataset:
+            for variable in dataset.variables:
+                if variable.startswith("stats_"):
+                    if variable not in variables:
+                        variables.append(variable)
+    return variables
+
+
+def read(variable, metric, area):
     ys = []
     xs = []
     for path in STATS_FILES:
@@ -71,15 +128,20 @@ def read_metric(metric):
             print("reading: {}".format(path))
             var = dataset.variables["time"]
             times = netCDF4.num2date(var[:], units=var.units)
-            strings = netCDF4.chartostring(dataset.variables["metric_names"][:])
-            metrics = [s.strip() for s in strings]
-            mi = metrics.index(metric.strip())
-            values = dataset.variables["stats_sst_all"][:, 0, 0, mi, 0]
+            mi = index(dataset.variables["metric_names"][:], metric)
+            ai = index(dataset.variables["area_names"][:], area)
+            values = dataset.variables[variable][:, 0, 0, mi, ai]
         xs.append(times)
         ys.append(values)
     x = np.ma.concatenate(xs)
     y = np.ma.concatenate(ys)
     return x, y
+
+
+def index(chars, item):
+    strings = netCDF4.chartostring(chars)
+    items = [s.strip() for s in strings]
+    return items.index(item.strip())
 
 
 if __name__.startswith('bk'):
