8f83499b27e28ce5f7ac95c3e9df63a72c6979cd
andrewgryan <andrew.ryan@metoffice.gov.uk>
2019-08-08 15:51:00 +0100

    add visible property to make l/c/r figure simple

--- switch_glyph/main.py
+++ switch_glyph/main.py
@@ -29,30 +29,56 @@ class Layer(object):
 
 
 class View(object):
+    """Manages glyphs related to a single layer on a figure"""
     def __init__(self, layer, figure, color):
         self.layer = layer
         self.figure = figure
         self.color = color
         self.glyphs = {}
+        self.key = None
+        self._visible = True
+
+    @property
+    def visible(self):
+        return self._visible
+
+    @visible.setter
+    def visible(self, flag):
+        try:
+            self.glyphs[self.key].visible = flag
+        except KeyError:
+            pass
+        self._visible = flag
 
     def on_change(self, attr, old, new):
         # Select/construct glyph_renderer on demand
-        glyph_key = self.glyph_key(new)
-        if glyph_key not in self.glyphs:
+        self.key = self.glyph_key(new)
+        if self.key not in self.glyphs:
             source = self.layer.get_source(new)
-            self.glyphs[glyph_key] = self.glyph(glyph_key, source)
+            self.glyphs[self.key] = self.glyph(self.key, source)
 
-        # Mute unselected glyphs
-        for key, glyph in self.glyphs.items():
-            glyph.visible = key == glyph_key
+        if self.visible:
+            # Mute unselected glyphs
+            for key, glyph in self.glyphs.items():
+                glyph.visible = key == self.key
+        else:
+            # Mute all glyphs
+            for glyph in self.glyphs.values():
+                glyph.visible = False
 
     def glyph(self, key, source):
         if key == "circle":
-            return self.figure.circle(x="x", y="y", source=source, fill_color=self.color)
+            return self.figure.circle(x="x", y="y", source=source,
+                    fill_color=self.color,
+                    visible=self.visible)
         elif key == "line":
-            return self.figure.line(x="x", y="y", source=source, line_color=self.color)
+            return self.figure.line(x="x", y="y", source=source,
+                    line_color=self.color,
+                    visible=self.visible)
         elif key == "square":
-            return self.figure.square(x="x", y="y", source=source, fill_color=self.color)
+            return self.figure.square(x="x", y="y", source=source,
+                    fill_color=self.color,
+                    visible=self.visible)
 
     @staticmethod
     def glyph_key(file_name):
@@ -92,29 +118,44 @@ def main():
         bokeh.plotting.figure(),
         bokeh.plotting.figure()
     ]
+    groups = []
     dropdowns = []
     loader = LayerLoader()
     for color in ["orange", "yellow", "blue"]:
         layer = Layer()
         dropdown = bokeh.models.Dropdown(menu=[(k, k) for k in file_names])
-
-        def on_change(layer, loader):
-            def callback(attr, old, new):
-                source = layer.get_source(new)
-                source.data = loader.load(new)
-            return callback
-
-        dropdown.on_change("value", on_change(layer, loader))
+        dropdown.on_change("value", pipe(layer, loader))
+        views = []
         for figure in figures:
             view = View(layer, figure, color=color)
             dropdown.on_change("value", view.on_change)
+            views.append(view)
         dropdowns.append(dropdown)
+
+        # Control L/R visibility
+        def on_change(views):
+            def on_change(attr, old, new):
+                for i, view in enumerate(views):
+                    view.visible = i in new
+            return on_change
+        group = bokeh.models.CheckboxButtonGroup(labels=["L", "R"])
+        group.on_change("active", on_change(views))
+        groups.append(group)
 
     column = bokeh.layouts.column(
             bokeh.layouts.row(*dropdowns),
+            bokeh.layouts.row(*groups),
             bokeh.layouts.row(*figures))
     document = bokeh.plotting.curdoc()
     document.add_root(column)
+
+
+def pipe(layer, loader):
+    """Connect sources to file system"""
+    def callback(attr, old, file_name):
+        source = layer.get_source(file_name)
+        source.data = loader.load(file_name)
+    return callback
 
 
 if __name__.startswith("bk"):
