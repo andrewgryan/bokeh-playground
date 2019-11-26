f593541865d905cb66d37b0245e819133ebf7d04
andrewgryan <andrew.ryan@metoffice.gov.uk>
2019-11-26 17:39:24 +0000

    add native-ish support for font-awesome buttons

---
+++ split-window/font_awesome_button.ts
@@ -0,0 +1,54 @@
+import {AbstractButton, AbstractButtonView} from "models/widgets/abstract_button"
+import {ButtonClick} from "core/bokeh_events"
+import * as p from "core/properties"
+
+// Extra imports
+import {button, i} from "core/dom"
+import {bk_btn, bk_btn_type} from "styles/buttons"
+
+
+export class FontAwesomeButtonView extends AbstractButtonView {
+  model: FontAwesomeButton
+
+  _render_button(...children: (string | HTMLElement)[]): HTMLButtonElement {
+      console.log(...children)
+      return button({
+          type: "button",
+          disabled: this.model.disabled,
+          class: [bk_btn, bk_btn_type(this.model.button_type)]
+      }, i({class: ["fas", "fa-cog"]}))
+  }
+
+  click(): void {
+    this.model.clicks = this.model.clicks + 1
+    this.model.trigger_event(new ButtonClick())
+    super.click()
+  }
+}
+
+export namespace FontAwesomeButton {
+  export type Attrs = p.AttrsOf<Props>
+
+  export type Props = AbstractButton.Props & {
+    clicks: p.Property<number>
+  }
+}
+
+export interface FontAwesomeButton extends FontAwesomeButton.Attrs {}
+
+export class FontAwesomeButton extends AbstractButton {
+  static __name__ = "FontAwesomeButton"
+  properties: FontAwesomeButton.Props
+
+  constructor(attrs?: Partial<FontAwesomeButton.Attrs>) {
+    super(attrs)
+  }
+
+  static init_FontAwesomeButton(): void {
+    this.prototype.default_view = FontAwesomeButtonView
+
+    this.override({
+      label: "FontAwesomeButton",
+    })
+  }
+}
--- split-window/main.py
+++ split-window/main.py
@@ -45,8 +45,34 @@ root = bokeh.layouts.layout(
     sizing_mode="stretch_both")
 document.add_root(root)
 
+
+from bokeh.core.properties import Override, Int
+from bokeh.events import ButtonClick
+class FontAwesomeButton(bokeh.models.AbstractButton):
+    __implementation__ = 'font_awesome_button.ts'
+
+    label = Override(default="FontAwesomeButton")
+    clicks = Int(0)
+    def on_click(self, handler):
+        ''' Set up a handler for button clicks.
+
+        Args:
+            handler (func) : handler function to call when button is clicked.
+
+        Returns:
+            None
+
+        '''
+        self.on_event(ButtonClick, handler)
+
+
+    def js_on_click(self, handler):
+        ''' Set up a JavaScript handler for button clicks. '''
+        self.js_on_event(ButtonClick, handler)
+
+
 gear = u"\u2699"
-btn = bokeh.models.Button(width=10, label=u"")
+btn = FontAwesomeButton(width=10)
 
 def on_click():
     print("Hello, settings!")
